b8974b7e7d744be16042bb62b246a34a49797e36
Pavel Stepanov <pavel@tomorrow.one>
2023-06-15 22:55:28 +0200

    Initial Commit

---
+++ WeatherCat/WeatherCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCatApp.swift
+//  WeatherCat
+//
+//  Created by Pavel Stepanov on 15.06.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
